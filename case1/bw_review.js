3737c0d69af1b7edc6b94ca06e90de7d0cf22477
Nicole Garay <ngaray157@west-mec.org>
2020-03-12 12:52:26 -0700

    finished case 1 :)

--- case1/bw_review.js
+++ case1/bw_review.js
@@ -39,7 +39,7 @@ window.onload = init;
 
 function init() {
    //stores object collection of the reviewing stars
-   var stars = document.querySelectorAll("span#stars");
+   var stars = document.querySelectorAll("span#stars img");
    for (var i = 0; i < stars.length; i++) {
       stars[i].style.cursor = "pointer";
       stars[i].addEventListener("mouseenter", lightStars);
@@ -47,39 +47,53 @@ function init() {
    document.getElementById("comment").addEventListener("keyup", updateCount);
 }
 
+
 function lightStars(e) {
    var starNumber = e.target.alt;
-   var stars = document.querySelectorAll("span#stars");
+   var stars = document.querySelectorAll("span#stars img");
    for (var i = 0; i < starNumber; i++) {
-      stars[i].e.target.src = "url(bw_star2.png)";
+      stars[i].src = "bw_star2.png";
    }
    for (var j = starNumber; j < 5; j++) {
-      stars[j].e.target.src = "url(bw_star.png)";
+      stars[j].src = "bw_star.png";
    }
-      document.getElementById("rating").value = starNumber + " stars";
-      e.target.addEventListener("mouseleave", turnOffStars);
-      e.target.addEventListener("mouseleave",
-         function () {
-            e.target.removeEventListener("mouseleave", turnOffStars);
-         }
-      )
-   
+   //this makes it so that when the mouse leaves the stars, they unhighlight
+   document.getElementById("rating").value = starNumber + " stars";
+   e.target.addEventListener("mouseleave", turnOffStars);
+   //when you click on the star, it removes the event listener of turnOffStars which keeps the color there
+   // to see how many stars you rate the movie
+   e.target.addEventListener("click",
+      function () {
+         e.target.removeEventListener("mouseleave", turnOffStars);
+      }
+   )
+
 }
 
-function turnOffStars() {
-   var stars = document.querySelectorAll("span#stars");
-   for (var i = 0; i < stars.length; i++) {
-      e.target.src = "url(bw_star.png)";
-      document.getElementById("rating").value = "";
+
+function turnOffStars(e) {
+   var stars = document.querySelectorAll("span#stars img");
+   for (var i = 0; i < 5; i++) {
+      stars[i].src = "bw_star.png";
+
    }
+   document.getElementById("rating").value = "";
 }
 
 //this function keeps track of how many characters the user has put into the text area box
-function updateCount(){
+function updateCount() {
    var commentText = document.getElementById("comment").value;
    var charCount = countCharacters(commentText);
-   var wordCountBox = document.querySelectorAll("input#wordCount");
-
+   var wordCountBox = document.getElementById("wordCount");
+   wordCountBox.value = charCount + "/1000";
+   //these if statements change the wordCount value to allow the user to see if they were over 1000 characters
+   if (charCount > 1000) {
+      wordCountBox.style.color = "white";
+      wordCountBox.style.backgroundColor = "red";
+   } else {
+      wordCountBox.style.color = "black";
+      wordCountBox.style.backgroundColor = "white";
+   }
 }
 
 
